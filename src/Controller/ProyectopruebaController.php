49087530794489ef8e19d23b8e4e5773d48f1e1b
Esteban De Jesus Rojas Solis <b66293@ecci.ucr.ac.cr>
2018-08-29 09:49:00 -0600

    primer commit

---
+++ src/Controller/ProyectopruebaController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class ProyectopruebaController extends AppController
+{
+    public function index()
+    {
+
+    }
+	
+	public function enrollments()
+    {
+
+    }
+	
+}
